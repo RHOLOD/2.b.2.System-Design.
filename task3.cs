2b3b55aa859fb463c71e0e1fb9a9deb09687e053
RHOLOD <42797219+RHOLOD@users.noreply.github.com>
2022-09-12 22:09:01 +0500

    Add files via upload

---
+++ task3.cs
@@ -0,0 +1,25 @@
+﻿//В С# легко реализуеться концепция "класс как модуль" с помощью абстактных классов.
+// Класс(модуль) MarineVessel автономен и уверсален, может использоваться как самостоятельный элемен - класс Boot
+// Класс(модуль) Sailboat использует родительский модуль и расширяет его дополнительными возможностями.
+abstract class MarineVessel
+{
+    public MarineVessel()
+    {
+
+    }
+    public void Drifting()
+    {
+        Console.WriteLine("Дрейвовать");
+    }
+}
+abstract class Boot :MarineVessel
+{
+
+}
+abstract class Sailboat : MarineVessel
+{
+    public void Sail()
+    {
+        Console.WriteLine("Плавать под парусом");
+    }
+}
