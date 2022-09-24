df06402527dba5f7b6d30cfe9040caf1e237e4a6
RHOLOD <42797219+RHOLOD@users.noreply.github.com>
2022-09-24 23:59:18 +0500

    Add files via upload

---
+++ task5.cs
@@ -0,0 +1,21 @@
+﻿Язык С# позволяет использование универсальных параметрах (generics) в методах и класах, 
+что поддерживает перый принцып повторного использования кода.
+class Person<T, K>
+{
+    public T Id { get; }
+    public K Password { get; set; }
+    public string Name { get; }
+    public Person(T id, K password, string name)
+    {
+        Id = id;
+        Name = name;
+        Password = password;
+    }
+}
+void Swap<T>(ref T x, ref T y)
+{
+    T temp = x;
+    x = y;
+    y = temp;
+}
+Второй и пятый принцыпы также можно реализовывать в языке С#
